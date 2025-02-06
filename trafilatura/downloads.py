c0d91854454ba7bf0237ee349b3d6154319a2d63
Matt Fleming <mattfleming@Matts-Mac-Studio.local>
2025-02-05 19:54:57 -0500

    here

--- trafilatura/downloads.py
+++ trafilatura/downloads.py
@@ -94,9 +94,21 @@ def create_pool(proxy: Optional[str] = None, **args: Any) -> Union[urllib3.PoolM
     return manager_class(**manager_args, **args)
 
 
-DEFAULT_HEADERS = urllib3.util.make_headers(accept_encoding=True)  # type: ignore[no-untyped-call]
-USER_AGENT = "trafilatura/" + version("trafilatura") + " (+https://github.com/adbar/trafilatura)"
-DEFAULT_HEADERS["User-Agent"] = USER_AGENT
+#DEFAULT_HEADERS = urllib3.util.make_headers(accept_encoding=True)  # type: ignore[no-untyped-call]
+#USER_AGENT = "trafilatura/" + version("trafilatura") + " (+https://github.com/adbar/trafilatura)"
+#DEFAULT_HEADERS["User-Agent"] = USER_AGENT
+DEFAULT_HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/109.0.0.0 Safari/537.36"
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+}
 
 FORCE_STATUS = [
     429,
